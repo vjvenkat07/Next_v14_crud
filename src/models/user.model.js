a522ecf77f6567ea3dc40fe5268e4e36b2b3bf26
Venkatesh <msdvjvenkat@gmail.com>
2024-07-03 11:40:33 +0530

    Added the user model

---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+const userModal = mongoose.model("Users", UserSchema);
+
+module.exports = userModal;
